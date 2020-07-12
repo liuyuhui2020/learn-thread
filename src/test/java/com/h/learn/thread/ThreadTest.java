aa7d4476797a1813fd886bc4e2e32cbc11ee85a2
liuyuhui <yuge7703@163.com>
2020-07-12 10:56:48 +0800

    测试提交忽略文件配置

--- src/test/java/com/h/learn/thread/ThreadTest.java
+++ src/test/java/com/h/learn/thread/ThreadTest.java
@@ -1,11 +1,11 @@
 package com.h.learn.thread;
 
-import com.h.learn.thread.runnable.RunnableThread;
+import com.h.learn.thread.runnable.RunnableThread1;
 import com.h.learn.thread.thread.Thread2;
 
 public class ThreadTest {
     public static void main(String[] args) {
-        RunnableThread thread1 = new RunnableThread();
+        RunnableThread1 thread1 = new RunnableThread1();
         Thread2 thread2 = new Thread2();
         thread1.run();
         thread2.start();
